41bfd9a728ab7fa92b1a09d532428ca679c782af
shentao <387944558@qq.com>
2016-08-09 15:32:01 +0800

    Initial commit

---
+++ Application/Home/Controller/AboutController.class.php
@@ -0,0 +1,18 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class AboutController extends BaseController {
+    public function index(){
+     $this->display();
+    }
+    public function contacts(){
+        $this->display();
+    }
+    public function join(){
+        $this->display();
+    }
+    public function report(){
+        $this->display();
+    }
+
+}
